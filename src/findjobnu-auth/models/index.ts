3d6be75a084d0d44b4e07ff8299475924c301b6d
Lucas <lucas.rosenorn@outlook.dk>
2025-06-29 23:42:45 +0200

    Task: Generate Auth Client

---
+++ src/findjobnu-auth/models/index.ts
@@ -0,0 +1,6 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AuthResponse';
+export * from './LoginRequest';
+export * from './RegisterRequest';
+export * from './TokenRefreshRequest';
